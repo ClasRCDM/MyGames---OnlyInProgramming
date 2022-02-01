f2855b50fb74b38ade5ff645bf368581e4457ff8
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-02-01 10:30:31 -0400

    grid e borda

--- testes/teste_grid_borda.py
+++ testes/teste_grid_borda.py
@@ -1,56 +1,69 @@
-import pygame
+from pygame import init, display, time, event, draw, QUIT
 from numpy import arange
 
 
 def grid(janela, comprimento, tamanho_linha, tamanho_quadrado):
-    """
-    :param janela: especifica a janela a ser usada.
-    :param comprimento: espaço que vai ocupar na tela.
-    :param tamanho_linha: até onde as linhas irão.
-    :param tamanho_quadrado: o tamanho do quadrado do grid.
-    :return: none
-    """
-
     def draw_grid(v):
-        pygame.draw.line(janela, (255, 255, 255),
-                         (v * tamanho_quadrado, 0),
-                         (v * tamanho_quadrado, comprimento))
-        pygame.draw.line(janela, (255, 255, 255),
-                         (0, v * tamanho_quadrado),
-                         (comprimento, v * tamanho_quadrado))
+        draw.line(janela, (255, 255, 255),
+                  (v * tamanho_quadrado, 0),
+                  (v * tamanho_quadrado, comprimento))
+        draw.line(janela, (255, 255, 255),
+                  (0, v * tamanho_quadrado),
+                  (comprimento, v * tamanho_quadrado))
 
     # Iniciando o grid
-    for x_c in arange(comprimento // tamanho_linha): draw_grid(x_c)
+    for x_c in arange(comprimento // tamanho_linha):
+        draw_grid(x_c)
 
 
-def borda(janela, comprimento, tamanho, espaçamento=0):
-    for x_c in arange(comprimento // tamanho):
-        pygame.draw.line(janela, (255, 255, 255),
-                         (x_c, espaçamento), (x_c, comprimento))
-    for y_c in arange(comprimento // tamanho):
-        pygame.draw.line(janela, (255, 255, 255),
-                         (espaçamento, y_c), (comprimento, y_c))
+def borda(janela, comprimento, tamanho, cor=(0, 0, 0), espaçamento=0, p_borda=12):
+    def borda_vertical(lado='esquerdo'):
+        for y in arange(comprimento // tamanho):
+            draw.line(janela, cor,
+                      (y if lado == 'esquerdo' else y+comprimento-p_borda,
+                       espaçamento),
+                      (y if lado == 'esquerdo' else y+comprimento-p_borda,
+                       comprimento))
+
+    def borda_horizontal(lado='cima'):
+        for x in arange(comprimento // tamanho):
+            draw.line(janela, cor,
+                      (espaçamento,
+                       x if lado == 'cima' else x+comprimento-p_borda),
+                      (comprimento,
+                       x if lado == 'cima' else x+comprimento-p_borda))
+
+    # -------------------------- Bordas
+    borda_vertical(lado='esquerdo')
+    borda_vertical(lado='direita')
+    borda_horizontal(lado='cima')
+    borda_horizontal(lado='baixo')
 
 
-pygame.init()
+# ------------------ Programa
+init()
 
 tela_cheia = 600, 600
 
-janela = pygame.display.set_mode(tela_cheia)
+janela = display.set_mode(tela_cheia)
 janela.fill((0, 0, 0))
-pygame.display.set_caption('Testes de grid')
+display.set_caption('Testes de grid')
 
 FPS = 60
-Fps = pygame.time.Clock()
+Fps = time.Clock()
 
-game = True
-while 1:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT: game = False
 
-    grid(janela, 400, 42, 50)
+def teste_grid(game):
+    while 1:
+        for evento in event.get():
+            if evento.type == QUIT:
+                game = False
 
-    pygame.display.flip()
-    Fps.tick(FPS)
+        grid(janela, 400, 42, 50)
+        borda(janela, 600, 30, (80, 80, 80), 0, 20)
 
-    if not game: break
+        display.flip()
+        Fps.tick(FPS)
+
+        if not game:
+            break
---
+++ testes/testes_init.py
@@ -0,0 +1,5 @@
+from teste_grid_borda import teste_grid
+
+game = True
+if __name__ == '__main__':
+    teste_grid(game)
