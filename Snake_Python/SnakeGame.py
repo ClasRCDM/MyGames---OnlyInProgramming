1e527fef3dca24ff0c3704fbc23f26ce7249450a
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-02-04 08:05:36 -0400

    update 2.2.1
    colisão do mouse com o texto, verificando click no jogar e sair

--- Snake_Python/SnakeGame.py
+++ Snake_Python/SnakeGame.py
@@ -12,60 +12,115 @@ from módulos import texto
 ########################
 
 class Mundo:
-    # Build game
+    # Build game &
     pygame.init()
     pygame.display.set_caption('Mundo...')
 
-    # Basic utilities
+    # Basic utilities &
     janela = pygame.display.set_mode((TELA_CHEIA), depth=32, display=1)
     fps = pygame.time.Clock()
 
+    # World variables &
     FPS = None
     ativo_m = None
+    mouse_pos = None
+    clicando = None
 
+    # &#########################& #
     @classmethod
     def variáveis_inicias(cls, ativo_m=True, cor_fundo=(0, 0, 0), fps=60):
+        # @ Important variables @ #
         cls.ativo_m = ativo_m
         cls.janela.fill(cor_fundo)
         cls.FPS = fps
 
+        if cls.mouse_pos is None:
+            cls.mouse_pos = pygame.mouse.get_pos()
+
     @classmethod
     def principais_eventos(cls):
         for evento in pygame.event.get():
-            # Exit to game
-            if evento.type == pygame.QUIT and cls.ativo_m:
-                cls.ativo_m = False
+            # Exit to game &
+            cls.ativo_m = cls.fechando_janela(evento)
+            cls.clicando = cls.checando_click(evento)
 
         pygame.display.flip()
         cls.fps.tick(cls.FPS)
 
+    @classmethod
+    def checando_click(cls, evento):  # @ Checking the click @ #
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
+            return True
+        if evento.type == pygame.MOUSEBUTTONUP and evento.button == 1:
+            return False
+
+    @classmethod
+    def fechando_janela(cls, evento):  # @ Closing window @ #
+        if evento.type == pygame.QUIT and cls.ativo_m:
+            return False
+        return True
+    # &#########################& #
+
     def __init__(self):
         print('Mundo iniciado')
 
+        #################
+        # /Itens World\ #
+
+        # $ Texts $ #
+        self.Textos: dict = {}
+
+        # \Itens World/ #
+        #################
+
     def iniciando(self):
-        self.menu(self.janela)
+        self.menu_estatico(self.janela)
         self.processando()
 
     def processando(pcs):
         while 1:
+            pcs.mouse_pos = pygame.mouse.get_pos()
+
             pcs.principais_eventos()
+            pcs.menu_com_input(pcs.janela)
 
             if not pcs.ativo_m: break
 
-    def menu(init, janela):
+    def menu_estatico(init, janela):
         defs.borda(janela, TELA_CHEIA[0], 30, (80, 80, 80), 0, 20)
 
-        # Title
-        título = texto.Texto(janela, 'Snake_Pygame', 30, (255, 255, 255),
+        # % Title %
+        título = texto.Texto(janela, 'Snake_Pygame', 30,
+                             cor=(255, 255, 255),
                              x=TELA_CHEIA[0] / 2,
                              y=TELA_CHEIA[1] / 2 - 100)
         título.sublinhado(posição='cima', caractere='*')
-        # Play game
-        b_jogar = texto.Texto(janela, 'Jogar', 40, (255, 255, 255),
+
+        # % Play game %
+        b_jogar = texto.Texto(janela, 'Jogar', 40, 210, 50,
+                              rect=True,
+                              cor=(255, 255, 255),
+                              cor_fundo=(10, 10, 10),
                               x=TELA_CHEIA[0] / 2,
                               y=TELA_CHEIA[1] / 2 + 50)
         b_jogar.sublinhado(posição='cima_baixo')
-        # Exit
-        texto.Texto(janela, 'Sair', 35, (255, 255, 255),
-                    x=TELA_CHEIA[0] / 2,
-                    y=TELA_CHEIA[1] / 2 + 150).sublinhado(posição='cima_baixo')
+
+        # % Exit %
+        b_sair = texto.Texto(janela, 'Sair', 35, 150, 45,
+                             rect=True,
+                             cor=(255, 255, 255),
+                             cor_fundo=(10, 10, 10),
+                             x=TELA_CHEIA[0] / 2,
+                             y=TELA_CHEIA[1] / 2 + 150)
+        b_sair.sublinhado(posição='cima_baixo')
+
+        # * Results *
+        init.Textos['b_jogar'] = b_jogar
+        init.Textos['b_sair'] = b_sair
+
+    def menu_com_input(update, janela):
+        if update.clicando:  # Starting game or Leaving &
+            if update.Textos['b_jogar'].mouse_colisão(update.mouse_pos):
+                print('jogar')
+            elif update.Textos['b_sair'].mouse_colisão(update.mouse_pos):
+                print('sair')
--- Snake_Python/módulos/texto/__init__.py
+++ Snake_Python/módulos/texto/__init__.py
@@ -1,7 +1,8 @@
 # () /Imports text\ () #
-from pygame import font  # Text initialization
+from pygame import font, Rect, draw  # Text initialization
 from os import path, getcwd  # Get files
 from variáveis import ARQUIVO_FONT, NOME_FONT  # source location
+from numpy import arange
 # () \Imports text/ () #
 
 
@@ -10,34 +11,44 @@ class Texto:  # Texts
     font.init()
     diretorio: str = path.join(getcwd(), ARQUIVO_FONT, NOME_FONT)
 
-    def __init__(self, janela, txt: str,
-                 tamanho: int, cor: tuple,
+    def __init__(self, janela, txt: str, tamanho: int,
+                 largura: int = 0, altura: int = 0,
+                 cor: tuple = (0, 0, 0),
+                 cor_fundo: tuple = (0, 0, 0),
                  x: int = 0, y: int = 0,
-                 not_pixel: bool = False):
+                 not_pixel: bool = False,
+                 rect: bool = False):
         # &#########################& #
         self.tamanho: int = tamanho
 
         self.txt: str = txt
         self.pixel: bool = not_pixel
+        self.rect: bool = rect
+
         self.cor: tuple = cor
+        self.cor_fundo: tuple = cor_fundo
 
         self.x, self.y = x, y
+        self.altura, self.largura = altura, largura
 
         self.janela = janela
         # &#########################& #
+
+        # Text Variables
+        self.colisão_mouse = None
 
         self.draw()
 
     def sublinhado(text, posição='baixo',
                    cor=('letra', (0, 0, 0)),
                    caractere='_'):  # $ Leaving underlined $ #
-        txrender, txfont = text.font(0), text.font(1)
+        txrender, txfont = text.fonts(0), text.fonts(1)
         sublinhado = txfont.render(caractere * len(text.txt), text.pixel,
                                    text.cor if cor[0] == 'letra' else cor[1])
         posi = None
         if posição == 'baixo': posi = text.y - txrender.get_rect().height
         elif posição == 'cima': posi = text.y - txrender.get_rect().height - 60
-        elif posição == 'cima_baixo':
+        if posição == 'cima_baixo':
             posi = text.y - txrender.get_rect().height
             text.janela.blit(sublinhado, (text.x - txrender.get_rect().width / 2,
                                           text.y - txrender.get_rect().height - 60))
@@ -45,14 +56,29 @@ class Texto:  # Texts
         text.janela.blit(sublinhado, (text.x - txrender.get_rect().width / 2, posi))
 
     def render(rd):  # $ To create $ #
-        txrender = rd.font(0)
+        txrender = rd.fonts(0)
+
+        if rd.rect:
+            draw.rect(rd.janela, rd.cor_fundo,
+                      ((rd.x - txrender.get_rect().width / 2)-10, (rd.y - 50)-10,
+                       rd.largura+4, rd.altura+8), 0)
         rd.janela.blit(txrender, (rd.x - txrender.get_rect().width / 2,
                                   rd.y - 50))
 
-    def font(rd, index=0):  # $ Set variables $ #
+    def fonts(rd, index=0):  # $ Set variables $ #
         txt_font = font.Font(rd.diretorio, rd.tamanho)
         txt_render = txt_font.render(rd.txt, rd.pixel, rd.cor)
         return txt_render if index == 0 else txt_font
 
     def draw(self):  # $ Render $ #
         self.render()
+
+    def mouse_colisão(self, posição):
+        txrender = self.fonts(0)
+        x_txt = (self.x - txrender.get_rect().width / 2)-10
+        y_txt = self.y - 50
+
+        if posição[0] > x_txt and posição[0] < x_txt + self.largura+4:
+            if posição[1] > y_txt and posição[1] < y_txt + self.altura+8:
+                return True
+        return False
